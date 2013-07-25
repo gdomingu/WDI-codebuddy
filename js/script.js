a491d6acb70008106ab26926c76aea8f14c51b23
Gabe <gabedominguez@outlook.com>
2013-07-25 15:59:19 -0700

    responsive nav

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  var links = $('#container>header a');
+  links.hide();
+  $('#hamburger').on('click', function(){
+    links.toggle('showOrHide');
+    $('body').toggleClass('active');
 
-})
+  });
+});
